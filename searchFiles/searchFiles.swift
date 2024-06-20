0d81e2a1e0b43904dc426367ed7b4f9e7b21b192
Dean Pulsifer <dean.pulsifer@icloud.com>
2024-06-19 17:23:44 -0700

    Initial FileManager implementation

--- searchFiles/searchFiles.swift
+++ searchFiles/searchFiles.swift
@@ -5,15 +5,26 @@
 //  Created by Dean Pulsifer on 6/18/24.
 //
 
+import Foundation
 import ArgumentParser
 
 @main
 struct searchFiles: ParsableCommand {
-    @Argument var searchPath: String
+    @Argument (help: "path to search")
+    var searchPath: String?
     
     mutating func run() throws {
-            print("searchFiles !!!")
-                
+        let fileManager = FileManager.default
+        let pwd = fileManager.currentDirectoryPath
+        print("The current path is \(pwd)")
+        
+        var actualPath: String = pwd
+        if let argString = searchPath {
+            print("Path argument is \(argString)")
+            actualPath = argString
         }
+        print("Actual path to start the search \(actualPath)")
+    }
+    
 }
 
