edf6a2dd8264352b27934003ac49d52c25973231
Dean Pulsifer <dean.pulsifer@icloud.com>
2024-06-22 09:22:30 -0700

    Got async version working.  Still needs some clean up

--- searchFiles/searchFiles.swift
+++ searchFiles/searchFiles.swift
@@ -9,11 +9,11 @@ import Foundation
 import ArgumentParser
 
 @main
-struct searchFiles: ParsableCommand {
+struct searchFiles: AsyncParsableCommand {
     @Argument (help: "path to search")
     var searchPath: String?
     
-    mutating func run() throws {
+    mutating func run() async throws {
         let fileManager = FileManager.default
         let pwd = fileManager.currentDirectoryPath
         print("The current path is \(pwd)")
@@ -31,26 +31,52 @@ struct searchFiles: ParsableCommand {
         if let file_enumerator = fileManager.enumerator(at: actualURL, includingPropertiesForKeys: Array(resourceKeys),
                                                         options: [.skipsHiddenFiles],
                                                         errorHandler: { (url, error) -> Bool in
-                                                            print("directoryEnumerator error at \(url): ", error)
+            print("File enumerator error at \(url): ", error)
             return true} ) {
-            for case let fileURL as URL in file_enumerator {
-                guard let resourceValues = try? fileURL.resourceValues(forKeys: resourceKeys),
-                      let isDirectory = resourceValues.isDirectory,
-                      let name = resourceValues.name,
-                      let isRegularFile = resourceValues.isRegularFile,
-                      let fileSize = resourceValues.fileSize
-                else {
-                    continue
-                }
-                // determine which type of URL we are accessing
-                if isDirectory {
-                    if name == "_extras" {
-                        file_enumerator.skipDescendants()
+            // try using a TaskGroup
+            let results = try await withThrowingTaskGroup(of: (String, Bool).self, returning: [String: Bool].self) { taskgroup in
+                for case let fileURL as URL in file_enumerator {
+                    guard let resourceValues = try? fileURL.resourceValues(forKeys: resourceKeys),
+                          let isDirectory = resourceValues.isDirectory,
+                          let name = resourceValues.name,
+                          let isRegularFile = resourceValues.isRegularFile,
+                          let fileSize = resourceValues.fileSize
+                    else {
+                        continue
                     }
-                } else if isRegularFile {
-                    print("File to search: \(fileURL.path()) with size \(fileSize)")
+                    // determine which type of URL we are accessing
+                    if isDirectory {
+                        if name == "_extras" {
+                            file_enumerator.skipDescendants()
+                        }
+                    } else if isRegularFile {
+                        print("File to search: \(fileURL.path()) with size \(fileSize) before addTask")
+                        taskgroup.addTask  {
+                            let filePath = fileURL.path()
+                                print("  Before searchLineByLine()")
+                                let foundString = try await searchLineByLine(from: fileURL, with: "small")
+                                print("  After searchLineByLine() await")
+                                if foundString {
+                                    print("  File \(fileURL.standardizedFileURL) contains search string")
+                                }
+                                print("  After checking foundString result")
+                            return (filePath, foundString)
+                        }
+                        print("after task, but inside for loop for file enumerator")
+                    }
                 }
+                // return results from TaskGroup here
+                print("Gather child task results to return via TaskGroup")
+                var childTaskResults = [String: Bool]()
+                for try await result in taskgroup {
+                    childTaskResults[result.0] = result.1
+                }
+                return childTaskResults
             }
+            print("after for loop for file enumerator")
+            // Iterate through found results
+            print("Search Results:")
+            results.forEach { print($0) }
         } else {
             print("**** File Enumerator was nil !!!")
         }
@@ -58,3 +84,24 @@ struct searchFiles: ParsableCommand {
     
 }
 
+func searchLineByLine(from fileUrl: URL, with searchString: String) async throws -> Bool {
+    print("    Entering searchLineByLine with \(fileUrl.standardizedFileURL) for string \(searchString)")
+    var found: Bool = false
+    do {
+        print("    inside do catch for searchLineByLine")
+        let handle = try FileHandle(forReadingFrom: fileUrl)
+        print("    After getting handle for \(fileUrl.standardizedFileURL)")
+        for try await line in handle.bytes.lines {
+            print("    processing: \(line)")
+            if line.contains(searchString) {
+                found = true
+                break
+            }
+        }
+        try handle.close()
+    } catch {
+        print("*** searchLineByLine getting file handle error \(error)")
+    }
+    print("    Exiting searchLineByLine with result \(found)")
+    return found
+}
